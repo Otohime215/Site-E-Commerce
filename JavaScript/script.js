4495e310cb4d09fa7942727ef942fc9b6a9eddbb
Luiz Guilherme Macêdo de Araujo <168070378+Otohime215@users.noreply.github.com>
2024-10-14 20:13:12 -0300

    Site E-Commerce

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+    /*const navbar = document.querySelector('.navbar');
+    const menuHamburguer = document.querySelector('.menu-hamburguer');
+
+        menuHamburguer.addEventListener('click', () => {
+            navbar.classList.toggle('show-menu');
+        });*/
+
+const navbar = document.querySelector('.navbar');
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const nav = document.querySelector('nav');
+
+menuHamburguer.addEventListener('click', () => {
+    navbar.classList.toggle('show-menu');
+});
